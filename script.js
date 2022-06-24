ef19bf2162087e8d4bd1617a381657b6f8b0bc3d
MateusDiniz <mateus.090998@gmail.com>
2022-06-24 18:35:11 -0300

    Completo + sidebar completos

--- script.js
+++ script.js
@@ -20,16 +20,12 @@ function escreverMensagem(){
 function abrirSidebar(){
     let sidebar = document.querySelector(".sidebar");
     sidebar.classList.add("mostrar-sidebar");
-    document.querySelector(".top").classList.add("opaco");
-    document.querySelector(".chat").classList.add("opaco");
-    document.querySelector(".bot").classList.add("opaco");
+    document.querySelector(".tela").classList.remove("hidden");
 }
 function fecharSidebar(){
     let sidebar = document.querySelector(".sidebar");
     sidebar.classList.remove("mostrar-sidebar");
-    document.querySelector(".top").classList.remove("opaco");
-    document.querySelector(".chat").classList.remove("opaco");
-    document.querySelector(".bot").classList.remove("opaco");
+    document.querySelector(".tela").classList.add("hidden");
 }
 
 function cadastrarUsuario(){
